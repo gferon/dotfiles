d42e04e44a48dcd17da375a166c931c63acdfae4
Gabriel Féron <gabriel.feron@here.com>
2018-09-29 19:07:20 +0200

    Update to latest version, remove atom and switch fork of i3-gnome

--- install.sh
+++ install.sh
@@ -12,17 +12,6 @@ if [ -x $(command_exists i3) ]; then
   ln -s $PWD/i3 ~/.config/i3
   ln -s $PWD/i3status ~/.config/i3status
   echo "if you didn't do it already, you should cd i3-gnome; run make install"
-fi
-
-if [ -x $(command_exists apm) ]; then
-  echo "atom package manager is installed, installing plugins"
-  apm install language-ansible
-  apm install language-nginx
-  apm install language-logstash
-  apm install git-time-machine
-  apm install minimap
-  apm install pigments
-  apm install firewatch-syntax
-  echo "atom is installed, symlinking configuration"
-  ln -sf $PWD/atom/config.cson ~/.atom/config.cson
+  cd i3-gnome
+  PREFIX=/usr/local sudo -E make install
 fi
