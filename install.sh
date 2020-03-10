c502175632c7236c0b13c29406ca4215133cd7f8
Gabriel <feron.gabriel@gmail.com>
2020-03-10 09:38:25 +0100

    More goodies

--- install.sh
+++ install.sh
@@ -17,14 +17,17 @@ exists playerctl
 exists j4-dmenu-desktop
 exists alacritty
 exists mako
+exists wofi
 
 echo "symlinking configuration"
 set -x
 mkdir -p ~/.config/{sway,swaylock}
 ln -sf $PWD/sway/* ~/.config/sway/
 ln -sf $PWD/swaylock/* ~/.config/swaylock/
-ln -sf $PWD/zprofile ~/.zprofile
+ln -sf $PWD/zprofile ~/.zprofile  # for zsh
+ln -sf $PWD/zprofile ~/.profile   # for bash
 swaymsg reload
 
 echo "getting wallpapers"
 curl -Lfo ~/.config/swaylock/lock.png http://static.simpledesktops.com/uploads/desktops/2020/01/19/Uno_Xray.png
+
--- zprofile
+++ zprofile
@@ -1,6 +1,20 @@
+# This will be evaluated before sway starts
+# so the sway process itself has this environment
 if [ -n "$DESKTOP_SESSION" ]; then
+  # Secrets management
   eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
   export SSH_AUTH_SOCK
+
+  # in general, we light dark things
+  export GTK_THEME="Adwaita-dark"
+
+  # force Firefox to use Wayland
   export MOZ_ENABLE_WAYLAND=1
+
+  # Rust
+  export PATH=~/.local/bin:$PATH
+
+  # Python & co
+  export PATH=~/.cargo/bin:$PATH
 fi
 
